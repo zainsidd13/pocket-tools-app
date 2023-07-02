d18b9568cd5cedd2c1305d2e8465a97cadd2b2c8
Zain Siddiqui <zainsidd13@hotmail.com>
2023-07-02 02:23:46 -0400

    Light and Dark Mode fully functional with button

--- PocketTools/src/App.tsx
+++ PocketTools/src/App.tsx
@@ -1,14 +1,23 @@
 import HomePage from './pages/HomePage';
+import { Switch, FormGroup, FormControlLabel } from '@mui/material';
+import { SyntheticEvent, useState } from 'react';
+
 
 function App() {
+  const [mode, setMode] = useState("")
+  const handleModeChange = (e: SyntheticEvent) => {
+    e.preventDefault();
+    mode ? setMode("") : setMode("light");
+    console.log(mode);
+  }
 
   return (
-    <html className='light'>
-      <body>
-        <HomePage />
-      </body>
-    </html>
-
+    <body className={mode}>
+      <HomePage mode={mode}/>
+      <FormGroup>
+        <FormControlLabel control={<Switch onChange={handleModeChange}/>} label={mode ? "Light" : "Dark"} />
+      </FormGroup>
+    </body>
   )
 }
 
--- PocketTools/src/components/Tool.tsx
+++ PocketTools/src/components/Tool.tsx
@@ -3,21 +3,24 @@ import ShareIcon from '@mui/icons-material/Share';
 interface ToolProps {
     title?: string;
     icon?: React.ElementType;
-    link?: string
+    link?: string;
+    mode?: string
   }
 
-function Tool({ title = 'To Fill', icon: IconComponent, link}: ToolProps) {
+function Tool({ title = 'To Fill', icon: IconComponent, link, mode}: ToolProps) {
     const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 
     return (
     
     <a href={link} target="_blank">
-        <div className='tool-card' style={{ border: `1px solid ${randomColor}` }}>
-            <div className='share-icon'>
-                <ShareIcon />
-            </div>
-            {IconComponent && <IconComponent sx={{ fontSize: 70, color: 'white' }} className='card-icon' />}
-            <h2 className='card-title'>
+        <div className='tool-card' style={mode ? {border : '1px solid black'} : { border: `1px solid ${randomColor}` }}>
+                <div className='share-icon'>
+                    <a href="#">
+                        <ShareIcon />
+                    </a>
+                </div>
+            {IconComponent && <IconComponent sx={{ fontSize: 70, color: 'white' }} className='card-icon' style={mode ? {color : 'black'} : {color : 'white'}}/>}
+            <h2 className='card-title' style={mode ? {color : 'black'} : {color : 'white'}}>
                 {title}
             </h2>
         </div>
